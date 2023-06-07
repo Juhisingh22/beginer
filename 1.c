b6a02dda1d0c4e14a2b1fb52a25cc9b06aaad4c6
Juhisingh22 <135867182+Juhisingh22@users.noreply.github.com>
2023-06-07 23:35:56 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+/*#- *preprocessing command*/
+//This is the first line of our code.
+//For example, if our programs needs mathematical operations of high level them we must include:
+
+#include <math.h>
+int main()
+// from here program begins
+//this is the 2nd line.
+//int is the return type of function and the return type is according to the functions activity
+{
+int a;
+    printf("hello world");
+//simply a print statement.
+///n is the new line character here.
+  scanf("%d",&a);
+ //scanf is used to take inputs from the user
+ return 0;
+ //Return 0 means that the function is working perfectly.
+}
+/*A C program is made up of different tokens combined together. These tokens include:
+
+Keywords -are reserved words
+Identifiers-names given to variables or functions
+Note: C is a case sensitive language so an identifier containing a capital letter and another one containing a small letter at the same place will be different
+Constants -difference between constant and variable is that a constant’s value never changes
+String Literal - a line of characters enclosed by double quotes
+Symbols - special characters reserved to perform certain actions
+*/
+
+//Variables & Data Types In C
+/*
+- We cannot declare a variable without specifying its data type. 
+*/
+
+//Operators In C
+/*
+- Special symbols that are used to perform actions or operations are known as operators.”
+>Arithmetic operators 
+-are used to perform mathematical operations such as
+ addition, subtraction etc
+ >Relational operators 
+ -are used for the comparison between two or more numbers
+> There are three logical operators
+- i.e. AND, OR and NOT. 
+> Assignment operators are used to assign values.
+*/
+
+
+/*
+Format specifier in C:-
+The format specifier in C programming is used for input and output purposes.
+Some examples are %d, %c, %f, etc.
+*/
+
+/* 
+> if else 
+if ( condition ){ 
+statements;}
+ else {
+statements;}
+
+> nested if else
+*/
